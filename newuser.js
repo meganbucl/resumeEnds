2452bfbde4a59e054ab56a91d6fa673a7addf6b4
Ariana MacArthur <ariana.macarthur@nwktc.edu>
2020-12-02 10:45:08 -0700

    AHHHHHH

---
+++ newuser.js
@@ -0,0 +1,15 @@
+var createuser = document.getElementById('User');
+var firstname = document.getElementById('firstname');
+var lastname = document.getElementById('lastname');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+
+createuser.addEventListener("click", ()=>{
+  let user = `http://megan.local:5159/newUser/${username.value}/${password.value}/${firstname.value}/${lastname.value}`
+  fetch(user)
+      .then(response => response.json())
+      .then(data => {
+        window.location.assign("index.html");
+        console.log(user);
+      });
+});
