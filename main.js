78709c24d121d963f6a0fc66a172b157a033f056
Ariana MacArthur <ariana.macarthur@nwktc.edu>
2020-12-02 09:40:22 -0700

    FML

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ var login = document.getElementById('Resume');
 var user = document.getElementById('username');
 var password = document.getElementById('password');
 
-var submit = document.getElementById('SubMit');
-var firstName = document.getElementById('firstName');
-var username = document.getElementById('username');
-var position = document.getElementById('position');
-var resume = document.getElementById('reume');
-
 login.addEventListener("click", ()=>{
   let url = `http://megan.local:5159/login/${user.value}/${password.value}`
   fetch(url)
@@ -15,15 +9,5 @@ login.addEventListener("click", ()=>{
       .then(data => {
         window.location.assign("resume.html")
         console.log(url);
-      });
-});
-
-submit.addEventListener("click", ()=>{
-  let resume = `http://megan.local:5159/sendResume/${username.value}/${firstName.value}/${position.value}/${resume.value}`
-  fetch(resume)
-      .then(response => response.json())
-      .then(data => {
-        window.location.assign("SubmitPage.html")
-        console.log(resume);
       });
 });
---
+++ resume.js
@@ -0,0 +1,15 @@
+var submit = document.getElementById('SubMit');
+var firstName = document.getElementById('firstName');
+var username = document.getElementById('username');
+var position = document.getElementById('position');
+var resume = document.getElementById('reume');
+
+submit.addEventListener("click", ()=>{
+  let resume = `http://megan.local:5159/sendResume/${username.value}/${firstName.value}/${position.value}/${resume.value}`
+  fetch(resume)
+      .then(response => response.json())
+      .then(data => {
+        window.location.assign("SubmitPage.html")
+        console.log(resume);
+      });
+});
